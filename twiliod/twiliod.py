c5108e62dcb8ac78f175c4b71d2ceae59a3eeae8
Bruce Blore <bruceblore@protonmail.com>
2023-01-21 19:20:16 -0800

    Filled in helper function to determine next repeat day

--- twiliod/twiliod.py
+++ twiliod/twiliod.py
@@ -2,6 +2,8 @@ import json
 import os
 from twilio.rest import Client as TwilioClient
 from datetime import datetime
+import calendar
+calendar.setfirstweekday(calenar.SUNDAY)
 
 # Paths where things are stored
 CREDENTIAL_FILE = 'twiliod/credentials.json'    # Twilio credentials
@@ -44,13 +46,57 @@ def getFirstDate(reminder):
     ).timestamp()
 
 
+def fixWeekday(weekday):
+    # The calendar library uses 0 for Monday and 6 for Sunday. We use 0 for Sunday and 6 for Saturday. This fixes that
+    return (weekday + 1) % 7
+
+
+def addDays(timestamp, days):
+    # Add the given number of days to a timestamp
+    dt = datetime.fromtimestamp(timestamp)
+    nextDayOfMonth = dt.day + days
+    nextMonth = dt.month
+    nextYear = dt.year
+    while nextDayOfMonth > calendar.monthrange(nextYear, nextMonth):
+        nextDayOfMonth -= calendar.monthrange(nextYear, nextMonth)
+        nextMonth += 1
+        if nextMonth > 12:
+            nextMonth -= 12
+            nextYear += 1
+    return datetime(nextYear, nextMonth, nextDayOfMonth,
+                    dt.hour, dt.minute, dt.second).timestamp()
+
+
 def getNextDate(reminder, lastDate):
-    # Given a reminder and the last time it was sent, determine the next time it should be sent
-    pass
+    # Given a reminder and the last time it occured, return the next time it occurs
+    if reminder['recurrence'] == 'daily':
+        return addDays(lastDate, reminder['recurFreq'])
+
+    elif reminder['recurrence'] == 'weekly':
+        dt = datetime.fromtimestamp(lastDate)
+        dayOfWeek = fixWeekday(calendar.weekday(dt.year, dt.month, dt.day))
+
+        # If there are more reminders this week, return the time for the next one
+        if True in reminder['recurDays'][dayOfWeek + 1:]:
+            # Adding 1 because the zeroth element of the array is actually tomorrow!
+            return addDays(lastDate, reminder['recurDays'][dayOfWeek + 1:].index(True) + 1)
+
+        # If there are not more reminders this week, the next one is in the following number of days:
+        #   The number of days until Sunday (7 if today is Sunday) +
+        #   The number of days from Sunday to the first day of the week with the reminder (can be zero) +
+        #   7 * (recurFreq - 1) (skip 0 weeks if it should be every week, 1 week if it should be every 2 weeks, etc)
+        else:
+            daysUntilSunday = 7 - dayOfWeek
+            daysFromSunday = reminder['recurDays'].index(True)
+            return addDays(lastDate, daysUntilSunday + daysFromSunday + 7 * (reminder['recurFreq'] - 1))
 
 
-def deleteReminder(reminderID):
-    del reminders
+def save():
+    # Save the state of reminders and remindersSent
+    with open(REMINDER_CONFIG_FILE, 'w') as file:
+        file.write(json.JSONEncoder().encode(reminders))
+    with open(REMINDERS_SENT_FILE, 'w') as file:
+        file.write(json.JSONEncoder().encode(remindersSent))
 
 
 remindersToDelete = []
@@ -58,7 +104,7 @@ for reminderID in reminders.keys():
     reminder = reminders[reminderID]
 
     # If the reminder does not recur:
-    if reminder['recurrence'] == 'once':
+    if reminder['recurrence'] == 'once' or (reminder['recurrence'] == 'weekly' and True not in reminder['recurDays']):
         # If it should have been sent already, it should be sent then deleted. (We don't bother logging non-recurring reminders in reminersSent because they should be deleted immediately.)
         if getFirstDate(reminder) < datetime().now().timestamp():
             sendSMS(reminder['phone'], reminder['message'])
@@ -87,3 +133,10 @@ for reminderID in reminders.keys():
 
         # The loop terminates as soon as curTime is in the future. Schedule the future reminder
         schedule[curTime] = reminderID
+
+# Delete all reminders that we flagged for deletion
+for reminderID in remindersToDelete:
+    del reminders[reminderID]
+save()
+
+# Sleep until the first scheduled reminder
