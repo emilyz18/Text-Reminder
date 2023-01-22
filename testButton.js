698909c8f791acd1613ae9ea6cac4355a3cdc279
emilyz18 <105086688+emilyz18@users.noreply.github.com>
2023-01-21 19:22:14 -0800

    Update testButton.js

--- testButton.js
+++ testButton.js
@@ -1,76 +1,86 @@
 function addButton() {
-        var phoneNumber = document.getElementById("Phone Number").value;
-        var time = document.getElementById("Time").value;
-        var notificationTime = document.getElementById("Notification Time").value;
-        var content = document.getElementById("Content").value;
-        var phoneNumber = document.getElementById("Phone Number").value
-        var time = document.getElementById("Time").value
-        var notificationTime = document.getElementById("Notification Time").value
-        var content = document.getElementById("Content").value
-        var recurDays = [document.getElementById("Monday").value,
-                         document.getElementById("Tuesday").value,
-                         document.getElementById("Wednesday").value,
-                         document.getElementById("Thursday").value,
-                         document.getElementById("Friday").value,
-                         document.getElementById("Saturday").value,
-                         document.getElementById("Sunday").value]
-
-        const rtArray = [phoneNumber, time, notificationTime, content, recurDays]
-
-        // Convert array to JSON format
-        const keys = ["phoneNumber", "time", "notificationTime", "content", "recurDays"]
-        let jsonObj = convertToJSON(keys, rtArray);
+    document.getElementById("error").innerText = "must select at least one"
+    var phoneNumber = document.getElementById("Phone Number").value;
+    var time = document.getElementById("Time").value;
+    var notificationTime = document.getElementById("Notification Time").value;
+    var content = document.getElementById("Content").value;
+    var phoneNumber = document.getElementById("Phone Number").value
+    var time = document.getElementById("Time").value
+    var notificationTime = document.getElementById("Notification Time").value
+    var content = document.getElementById("Content").value
+    var recurDays = [document.getElementById("Monday").value,
+                     document.getElementById("Tuesday").value,
+                     document.getElementById("Wednesday").value,
+                     document.getElementById("Thursday").value,
+                     document.getElementById("Friday").value,
+                     document.getElementById("Saturday").value,
+                     document.getElementById("Sunday").value]
+                     const allEqual = recurDays.every( v => v === recurDays[0] )
+                     console.log(allEqual)
+    if (allEqual) {
+        document.getElementById("error").innerText = "must select at least one"
         
-        console.log(jsonObj);
+    } else {
+        document.getElementById("error").innerText = "must select at least one"
+
+    }
+
+    const rtArray = [phoneNumber, time, notificationTime, content, recurDays]
+
+    // Convert array to JSON format
+    const keys = ["phoneNumber", "time", "notificationTime", "content", "recurDays"]
+    let jsonObj = convertToJSON(keys, rtArray);
+    
+    console.log(jsonObj);
 }
 
 
 
 function convertToJSON(keys, values) {
-    let jsonObj = {};
+let jsonObj = {};
 
-    values.map((elem, index) => {
-        let key = keys[index];
-        return jsonObj[key] = elem;
-    })
+values.map((elem, index) => {
+    let key = keys[index];
+    return jsonObj[key] = elem;
+})
 
-    jsonObj = JSON.stringify(jsonObj);
+jsonObj = JSON.stringify(jsonObj);
 
-    return jsonObj;
+return jsonObj;
 }
 
 
 function checkbox(p){
-    document.getElementById(p).value = true
+document.getElementById(p).value = true
 }
 
 function changeFunc(i) {
-        let code = '<div id = "daysblock">'+
-        '<p>Select the Days you want the reminder to recur on: </p>' +
-      '<input type="checkbox" id="Monday" name="Monday" value=false onClick="checkbox(id)">' +
-      '<label for="Monday"> Monday</label><br><br>' +
-      '<input type="checkbox" id="Tuesday" name="Tuesday" value=false onClick="checkbox(id)">' +
-      '<label for="Tuesday"> Tuesday</label><br><br>' +
-      '<input type="checkbox" id="Wednesday" name="Wednesday" value=false onClick="checkbox(id)">' +
-      '<label for="Wednesday">Wednesday</label><br><br>' +
-      '<input type="checkbox" id="Thursday" name="Thursday" value=false onClick="checkbox(id)">' +
-      '<label for="Thursday">Thursday</label><br><br>' +
-      '<input type="checkbox" id="Friday" name="Friday" value=false onClick="checkbox(id)">' +
-      '<label for="Friday">Friday</label><br><br>' +
-      '<input type="checkbox" id="Saturday" name="Saturday" value=false onClick="checkbox(id)">' +
-      '<label for="Saturday">Saturday</label><br><br>' +
-      '<input type="checkbox" id="Sunday" name="Sunday" value=false onClick="checkbox(id)">' +
-      '<label for="Sunday">Sunday</label><br><br>'+
-      '</div>';
+    let code = '<div id = "daysblock">'+
+    '<p>Select the Days you want the reminder to recur on: </p>' +
+  '<input type="checkbox" id="Monday" name="Monday" value=false onClick="checkbox(id)">' +
+  '<label for="Monday"> Monday</label><br><br>' +
+  '<input type="checkbox" id="Tuesday" name="Tuesday" value=false onClick="checkbox(id)">' +
+  '<label for="Tuesday"> Tuesday</label><br><br>' +
+  '<input type="checkbox" id="Wednesday" name="Wednesday" value=false onClick="checkbox(id)">' +
+  '<label for="Wednesday">Wednesday</label><br><br>' +
+  '<input type="checkbox" id="Thursday" name="Thursday" value=false onClick="checkbox(id)">' +
+  '<label for="Thursday">Thursday</label><br><br>' +
+  '<input type="checkbox" id="Friday" name="Friday" value=false onClick="checkbox(id)">' +
+  '<label for="Friday">Friday</label><br><br>' +
+  '<input type="checkbox" id="Saturday" name="Saturday" value=false onClick="checkbox(id)">' +
+  '<label for="Saturday">Saturday</label><br><br>' +
+  '<input type="checkbox" id="Sunday" name="Sunday" value=false onClick="checkbox(id)">' +
+  '<label for="Sunday">Sunday</label><br><br>'+
+  '</div>';
 
 
-        if (i == 'weekly') {
-          document.getElementById("main").innerHTML += code;
-          document.getElementById("Recurence Dropdown").value = "weekly";
-        } else {
-            if ((document.getElementById("daysblock")) != null){
-            document.getElementById("daysblock").remove();
-            }
+    if (i == 'weekly') {
+      document.getElementById("main").innerHTML += code;
+      document.getElementById("Recurence Dropdown").value = "weekly";
+    } else {
+        if ((document.getElementById("daysblock")) != null){
+        document.getElementById("daysblock").remove();
         }
-        
-      }
+    }
+    
+  }
