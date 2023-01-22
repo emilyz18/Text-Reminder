9606eb3e008bc65ead322148b6fe1cd2726651ff
Swapnil-Dubey <dubeyswapnil8@gmail.com>
2023-01-21 17:36:51 -0800

    RecurDays showing

--- testButton.js
+++ testButton.js
@@ -3,14 +3,28 @@ function addButton() {
         var time = document.getElementById("Time").value;
         var notificationTime = document.getElementById("Notification Time").value;
         var content = document.getElementById("Content").value;
-        const rtArray = [phoneNumber, time, notificationTime, content]
+        var phoneNumber = document.getElementById("Phone Number").value
+        var time = document.getElementById("Time").value
+        var notificationTime = document.getElementById("Notification Time").value
+        var content = document.getElementById("Content").value
+        var recurDays = [document.getElementById("Monday").value,
+                         document.getElementById("Tuesday").value,
+                         document.getElementById("Wednesday").value,
+                         document.getElementById("Thursday").value,
+                         document.getElementById("Friday").value,
+                         document.getElementById("Saturday").value,
+                         document.getElementById("Sunday").value]
+
+        const rtArray = [phoneNumber, time, notificationTime, content, recurDays]
 
         // Convert array to JSON format
-        const keys = ["phoneNumber", "time", "notificationTime", "content"]
+        const keys = ["phoneNumber", "time", "notificationTime", "content", "recurDays"]
         let jsonObj = convertToJSON(keys, rtArray);
         
         console.log(jsonObj);
 }
+
+
 
 function convertToJSON(keys, values) {
     let jsonObj = {};
@@ -23,4 +37,9 @@ function convertToJSON(keys, values) {
     jsonObj = JSON.stringify(jsonObj);
 
     return jsonObj;
+}
+
+
+function checkbox(p){
+    document.getElementById(p).value = true
 }
